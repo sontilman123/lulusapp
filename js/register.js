8d1883702ca66fb2ce77a692e319d2b57cb5c7dd
mahdanahmad <mahdan.ahmad@yahoo.com>
2014-08-04 16:03:20 +0700

    webservice done

--- js/register.js
+++ js/register.js
@@ -1,14 +1,15 @@
 window.addEventListener('load',function() {
-	//alert("hula hula");
+	//alert("Hello! I am an alert box!");
 	
 	var submit = document.getElementsByName('submit')[0];
 	
-	var username = document.getElementById('login_username');
-	var password = document.getElementById('login_password');
+	var username = document.getElementById('registrasi_username');
+	var password = document.getElementById('registrasi_password');
 	
-	var error = "Username dan kata sandi tidak cocok";
+	var error1 = "Username sudah terpakai";
+	var error2 = "Registrasi gagal, mohon ulangi registrasi";
 	
-	document.getElementById('login_form').onsubmit = function() {
+	document.getElementById('registrasi_form').onsubmit = function() {
 		var xmlhttp = new XMLHttpRequest();
 		
 		var params = "?";
@@ -19,19 +20,22 @@ window.addEventListener('load',function() {
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
 				var response = JSON.parse(xmlhttp.responseText);
-				if (response['status'] == 'success') {
+				if (response['status'] == 'used') {
+					errortext.innerHTML = error1;
+					errortext.style.display = "inline";
+				} else if (response['status'] == 'success') {
 					localStorage.setItem("lulusapp_user", username.value);
 					window.location.href = 'home.html';
 				} else {
-					errortext.innerHTML = error;
+					errortext.innerHTML = error2;
 					errortext.style.display = "inline";
 				}
 			}
 		}
 		
-		xmlhttp.open("GET", "http://lombabbic.ap01.aws.af.cm/services/login.php" + params, true);
+		xmlhttp.open("GET", "http://lombabbic.ap01.aws.af.cm/services/register.php" + params, true);
 		xmlhttp.send(null);
-		
+		//alert("Hello! I am an alert box!");
 		return false;
 		
 		//localStorage.setItem("lulusapp_user", username.value);
